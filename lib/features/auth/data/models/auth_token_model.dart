7e52ed1d43e19f9986f3c3f5b0efb9eaf1c7a793
The Tormentor <the@tormentor.com>
2026-05-11 18:39:46 +0700

    fix(auth): make user field optional in AuthTokenModel to fix parsing error

--- lib/features/auth/data/models/auth_token_model.dart
+++ lib/features/auth/data/models/auth_token_model.dart
@@ -8,12 +8,12 @@ class AuthTokenModel {
   const AuthTokenModel({
     required this.accessToken,
     required this.refreshToken,
-    required this.user,
+    this.user,
   });
 
   final String accessToken;
   final String refreshToken;
-  final UserModel user;
+  final UserModel? user;
 
   factory AuthTokenModel.fromJson(Map<String, dynamic> json) => _$AuthTokenModelFromJson(json);
   Map<String, dynamic> toJson() => _$AuthTokenModelToJson(this);
